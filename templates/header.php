351eadc386fe1cc0d877cd2d2db53bff852fa48c
amm <f1smydrug@gmail.com>
2023-11-13 18:34:35 +0100

    add team first approach

--- templates/header.php
+++ templates/header.php
@@ -5,9 +5,32 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style type="text/css">
-
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+        select {
+            display: block;
+        }
     </style>
 </head>
 <body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="index.php" class="brand-logo brand-text">Duacode test</a>
+
+            <ul id="nav-mobile" class="right">
+                <li><a href="add_team.php" class="btn brand z-depth-0">Add a Team</a></li>
+            </ul>
+        </div>
+    </nav>
+
 
 
