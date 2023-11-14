42babe616467d15f48ced06c01577a203e042b8c
amm <f1smydrug@gmail.com>
2023-11-14 14:27:14 +0100

    create players ddbb table

--- config/sql_script.sql
+++ config/sql_script.sql
@@ -19,3 +19,29 @@ CREATE TABLE team (
 -- INSERT INTO `team` (`id`, `name`, `city`, `sport`, `created_at`) VALUES (NULL, 'Deportivo de La Coruña', 'La Coruña', 'Fútbol', current_timestamp());
 -- INSERT INTO `team` (`id`, `name`, `city`, `sport`, `created_at`) VALUES (NULL, 'McLaren', 'Woking', 'Formula 1', current_timestamp());
 -- INSERT INTO `team` (`id`, `name`, `city`, `sport`, `created_at`) VALUES (NULL, 'Ferrari', 'Maranello', 'Formula 1', current_timestamp());
+
+
+CREATE TABLE IF NOT EXISTS `player` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `number` INT NULL,
+    `team_id` INT NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `edited_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_teamId_Player_Team` 
+    FOREIGN KEY (`team_id`) 
+    REFERENCES `team` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci; 
+
+
+-- Only for testing 
+INSERT INTO `player` (`id`, `name`, `number`, `team_id`, `created_at`, `edited_at`) VALUES (NULL, 'Luka Modric', 10, 1, current_timestamp(), current_timestamp());
+INSERT INTO `player` (`id`, `name`, `number`, `team_id`, `created_at`, `edited_at`) VALUES (NULL, 'Brahim Díaz', 21, 1, current_timestamp(), current_timestamp());
+INSERT INTO `player` (`id`, `name`, `number`, `team_id`, `created_at`, `edited_at`) VALUES (NULL, 'Arda Güler', 24, 1, current_timestamp(), current_timestamp());
+INSERT INTO `player` (`id`, `name`, `number`, `team_id`, `created_at`, `edited_at`) VALUES (NULL, 'Oscar Piastri', 81, 3, current_timestamp(), current_timestamp());
+INSERT INTO `player` (`id`, `name`, `number`, `team_id`, `created_at`, `edited_at`) VALUES (NULL, 'Lando Norris', 4, 3, current_timestamp(), current_timestamp());
